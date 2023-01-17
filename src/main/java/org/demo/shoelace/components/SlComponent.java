ce86f625fbbf7949c11d4ac43a24e75ea351f555
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-01-17 15:49:05 +0100

    improve api

--- src/main/java/org/demo/shoelace/components/SlComponent.java
+++ src/main/java/org/demo/shoelace/components/SlComponent.java
@@ -1,6 +1,8 @@
 package org.demo.shoelace.components;
 
 import org.dwcj.annotations.InlineStyleSheet;
+import org.dwcj.interfaces.HasClassName;
+import org.dwcj.interfaces.HasStyle;
 import org.dwcj.webcomponent.WebComponent;
 
 /**
@@ -13,7 +15,7 @@ import org.dwcj.webcomponent.WebComponent;
 @InlineStyleSheet(id = "sl-common-styles", once = true, value = "" +
     "[sl-component]{overflow: visible}" +
     "[sl-component] .BBjHtmlView-content{overflow: visible}")
-public abstract class SlComponent<T extends WebComponent<T>> extends WebComponent<T> {
+public abstract class SlComponent<T extends WebComponent<T>> extends WebComponent<T> implements HasClassName, HasStyle {
 
   /**
    * Creates a new Shoelace component
@@ -21,5 +23,32 @@ public abstract class SlComponent<T extends WebComponent<T>> extends WebComponen
   public SlComponent() {
     super();
     getHtmlContainer().setAttribute("sl-component", "");
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public SlComponent<T> addClassName(String className) {
+    addComponentClassName(className);
+    return this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public SlComponent<T> removeClassName(String className) {
+    removeComponentClassName(className);
+    return this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public SlComponent<T> setStyle(String style, String value) {
+    setComponentStyle(style, value);
+    return this;
   }
 }
