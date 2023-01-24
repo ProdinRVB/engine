ffd12a6f75584b02730b1ff45a5047a165f726cd
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-01-24 15:53:11 +0100

    update ui5

--- src/main/java/org/demo/Ui5App.java
+++ src/main/java/org/demo/Ui5App.java
@@ -14,7 +14,6 @@ import org.dwcj.exceptions.DwcException;
     "  display: flex;" +
     "  align-items: center;" +
     "  justify-content: center;" +
-    "  flex-direction: column;" +
     "  gap: 1rem;" +
     "  height: 100%;" +
     "}")
@@ -26,14 +25,31 @@ public class Ui5App extends App {
     AppPanel panel = new AppPanel();
     panel.addClassName("app-panel");
 
-    Ui5Button button = new Ui5Button();
-    button.setText("Click Me");
-    button.addClickListener((event) -> {
-      consoleLog("Button Clicked");
-    });
+    Ui5Button defaults = new Ui5Button("Default");
+    defaults.setDesign(Ui5Button.Design.DEFAULT);
+    
+    Ui5Button transparent = new Ui5Button("Transparent");
+    transparent.setDesign(Ui5Button.Design.TRANSPARENT);
 
-    panel.add(button);
+    Ui5Button positive = new Ui5Button("Positive");
+    positive.setDesign(Ui5Button.Design.POSITIVE);
 
-    panel.add(panel);
+    Ui5Button negative = new Ui5Button("Negative");
+    negative.setDesign(Ui5Button.Design.NEGATIVE);
+
+    Ui5Button emphasized = new Ui5Button("Emphasized");
+    emphasized.setDesign(Ui5Button.Design.EMPHASIZED);
+
+    Ui5Button attention = new Ui5Button("Attention");
+    attention.setDesign(Ui5Button.Design.ATTENTION);
+
+    // loop over buttons and add click listener
+    Ui5Button[] buttons = {defaults, transparent, positive, negative, emphasized, attention};
+    for (Ui5Button button : buttons) {
+      button.addClickListener((event) -> {
+        consoleLog("Button " + button.getText() + " Clicked");
+      });
+      panel.add(button);
+    }
   }
 }
